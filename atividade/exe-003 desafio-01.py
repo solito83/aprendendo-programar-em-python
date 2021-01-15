cef1dc19bf219afd5e84540ed8de17cf169efa4b
solito83 <solito83@users.noreply.github.com>
2021-01-14 21:45:38 -0300

    py-002

--- atividade/exe-003 desafio-01.py
+++ atividade/exe-003 desafio-01.py
@@ -2,17 +2,16 @@ print('exe-003 desafios-01')
 print('Desafio: crie um script contendo o nome do usuario mais uma mensagem de boas vindas')
 
 nome=input('Qual é seu nome:')
-print('Seja bem ao Python,',nome)
+print('Seja bem ao Python, {}!'.format(nome))
 
 nome=input('Qual seu nome?')
 ano=input('Qual seu ano?')
 mes=input('Qual seu mês?')
 dia=input('Qual seu dia?')
-print('Muito prazer,',nome,'você nasceu em',dia,'de',mes,'de',ano)
+print('Muito prazer, {}!'.format(nome),'você nasceu em',dia,'de',mes,'de',ano)
 
-nuna=input('Digite um número:')
-nunb=input('Digite outro número:')
+nun1=int(input('Digite um número:'))
+nun2=int(input('Digite outrbnlko número:'))
+soma=nun1+nun2
 
-print('A soma é:',nuna+nunb)
-
-
+print('A soma é:',soma)
---
+++ atividade/exe-004 comandos primitivos.py
@@ -0,0 +1,9 @@
+print('exe-004 comandos primitivos')
+print('Tipos primitivos de comandos -int=inteiro- floot')
+
+num1=int(input('Digite um número'))
+num2=int(input('Digite outro número'))
+soma=num1+num2
+
+#print('A soma de',num1, 'e',num2, 'é',soma)
+print('A soma entre {} e {} vale {}'.format(num1, num2, soma))
