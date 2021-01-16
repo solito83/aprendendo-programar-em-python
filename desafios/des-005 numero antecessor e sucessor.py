9f9334e68728306f45d2224c92d3e3dd38993eb3
solito83 <solito83@users.noreply.github.com>
2021-01-16 18:50:01 -0300

    py-006

---
+++ desafios/des-005 numero antecessor e sucessor.py
@@ -0,0 +1,12 @@
+des = ('Desafio-005 número antecessor e sucessor')
+print ('{}.'.format(des))
+#Desafio-05 >>> Faça um programa que leia um numero inteiro e mostre na tela o seu antecessor e seu sucessor.
+
+n1 = int(input('Digite um número:'))
+n2 = (1)
+s1 = (n1+n2)
+s2 = (n1-n2)
+
+print ('Você digitou o número {}, \nO número antecessor dele é {} \nE seu sucessor é {}.'.format(n1, s2, s1))
+
+print ('The End')
---
+++ desafios/des-006 numero dobro triplo raiz.py
@@ -0,0 +1,11 @@
+des = ('Desafio-006 número dobro triplo raiz')
+print ('{}.'.format(des))
+#Desafio-06 >>> Crie um programa que leia um número e mostre o seu dobro, seu triplo e sua rais quadrada.
+
+n1 = int(input('Digite um número'))
+do = (n1*2)
+tr = (n1*3)
+rz = (n1**(1/2))
+print ('O dobro do número que você digitou é {}. \nE seu triplo é {}. \nJá sua raiz é {:.2f}.'.format(do, tr, rz))
+
+print ('The End')
