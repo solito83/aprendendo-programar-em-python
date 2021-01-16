e3dcb2b692c9017c90cd98487ae2112840030271
solito83 <solito83@users.noreply.github.com>
2021-01-16 16:23:24 -0300

    py-005

--- atividade/exe-007 desafios-03.py
+++ atividade/exe-007 desafios-03.py
@@ -21,9 +21,11 @@ di = n1//n2
 ex = n1**n2
 re = n1%n2
 
-print('A soma é: {}; o produto é: {}; e a divisão é: {}'.format(s, m, d))
-
-print('A divisão inteira é:{}; O expoente é:{}; o resto da divisão é:{};'.format(di, ex, re))
+print('A soma é: {}; o produto é: {}; e a divisão é: {:.3f}'.format(s, m, d))
+# Na divisão, para indicar o número de casas decimais podemos fazer da seguinte forma {:.2f} {:}=tag 2=o numero de casas decimais desejadas e f=float->flutuante.
+print('A divisão inteira é:{}; \nO expoente é:{}; \nO resto da divisão é:{};'.format(di, ex, re))
+# "\n = quebrar linha"
+# " end='' =continuar escrevendo na mesma linha".
 
 fim = ('The End.')
-print('{:^12}'.format(fim))
+print('{:>12}!'.format(fim))
---
+++ atividade/lista de desafios-002.py
@@ -0,0 +1,17 @@
+#Desafio-05 >>> Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+#Desafio-06 >>> Crie um programa que leia um número e mostre o seu dobro, seu triplo e sua rais quadrada.
+
+#Desafio-07 >>> Desenvolva um programa que leia duas notas de um aluno, calcule e mostre sua media.
+
+#Desafio-08 >>> Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+
+#Desafio-09 >>> Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada.
+
+#Desafio-10 >>> Crie um programa que leia qunto dinheiro na carteira tem uma pessoa e mostre na tela quantos dolares ela pode comprar.
+
+#Desafio-11 >>> Faça um programa que leia a largura e a altura de uma parede em metros. cálcule a sua area e a quantidade de tinta necessaria para pinta-la, sabendo que cada litro de tinta pinta dois metros quadrado.
+
+#Desafio-12 >>> Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+#Desafio-13 >>> Faça um algoritmo que leia o salário de funcionario e mostre seu novo salário com 15% de aumento.
