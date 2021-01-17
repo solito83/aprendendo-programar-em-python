09d1f0c9f3501a7ff5fad3a4d6a35b2f80151e85
solito83 <solito83@users.noreply.github.com>
2021-01-17 18:09:58 -0300

    py-10

--- aulas/exe-003 desafio-01.py
+++ aulas/exe-003 desafio-01.py
@@ -1,17 +1,15 @@
 print('exe-003 desafios-01')
 print('Desafio: crie um script contendo o nome do usuario mais uma mensagem de boas vindas')
 
-nome=input('Qual é seu nome:')
-print('Seja bem ao Python, {}!'.format(nome))
-
-nome=input('Qual seu nome?')
-ano=input('Qual seu ano?')
-mes=input('Qual seu mês?')
-dia=input('Qual seu dia?')
+nome=input('Qual é seu nome: ')
+ano=input('Qual seu ano? ')
+mes=input('Qual seu mês? ')
+dia=input('Qual seu dia? ')
 print('Muito prazer, {}!'.format(nome),'você nasceu em',dia,'de',mes,'de',ano)
 
-nun1=int(input('Digite um número:'))
-nun2=int(input('Digite outrbnlko número:'))
+nun1=int(input('Digite um número: '))
+nun2=int(input('Digite outro número: '))
 soma=nun1+nun2
+fim = 'The End'
 
-print('A soma é:',soma)
+print('A soma é: {}\n\n{}'.format(soma,fim))
