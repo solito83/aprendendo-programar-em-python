6e202433660fd3379fea07b7df0466f24f2cc797
solito83 <solito83@users.noreply.github.com>
2021-01-19 17:26:01 -0300

    py-teste

--- modulos/modulos.py
+++ modulos/modulos.py
@@ -1,8 +1,10 @@
-print ('aula 08 comandos import, math')
-print ('from math import sqrt')
-
+print('aula 08 comandos import, math')
+print('from math import sqrt')
+# modulo em python >>> bibliotecas externas, onde para criar um programa podemos importa comandos adiocionais destas bibliotecas
 import math
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
-print ('A rais de {} é igual a {}'.format(num, math.ceil(raiz)))
+print('A rais de {} é igual a {}'.format(num, math.ceil(raiz)))
+
+#importando emoji's
 
