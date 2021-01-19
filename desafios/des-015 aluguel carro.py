75fe358c33c13d2e3cac6c7c24d2c253d9ef8e97
solito83 <solito83@users.noreply.github.com>
2021-01-18 21:21:57 -0300

    py-014

--- desafios/des-015 aluguel carro.py
+++ desafios/des-015 aluguel carro.py
@@ -2,3 +2,11 @@ des = 'Aluguel de carro'
 print ('{}'.format(des))
 #Desafio-015 Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
 
+dia = int(input('Quantos dias você ficou com o veiculo? '))
+km = float(input('Quantos Km você rodou? '))
+
+pago = dia * 60 + km * 0.15
+fim = 'The End'
+
+print ('O total a pagar é de R${:.2f} reais\n{}'.format(pago,fim))
+
