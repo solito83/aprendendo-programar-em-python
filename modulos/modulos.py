2068f915726342f9bd97e6f4cdd1d514946aa782
solito83 <solito83@users.noreply.github.com>
2021-01-18 21:38:12 -0300

    py-015

--- modulos/modulos.py
+++ modulos/modulos.py
@@ -1,1 +1,8 @@
-print ('comandos import, math')
+print ('aula 08 comandos import, math')
+print ('from math import sqrt')
+
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print ('A rais de {} é igual a {}'.format(num, math.ceil(raiz)))
+
