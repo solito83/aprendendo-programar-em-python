689aa0ad0b4649d147c29bc741e030ba44c0a0e5
solito83 <solito83@users.noreply.github.com>
2021-01-22 17:12:33 -0300

    py-022

---
+++ modulos/des-021.py
@@ -0,0 +1,10 @@
+des = 'Música no python'
+print('{}'.format(des))
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('modulos\pushme.mp3')
+pygame.mixer.music.play()
+pygame.mixer.music.stop()
+
---
+++ musicas/musicas.py
@@ -0,0 +1,8 @@
+print ('Musicas')
+import pygame
+pygame.init()
+
+
+pygame.mixer_music.load('musicas\pushme.mp3')
+pygame.mixer_music.play()
+#Não funcionou pesquisar mais se for necessario usar este código.
