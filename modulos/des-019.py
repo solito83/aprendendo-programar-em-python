d952f5672e2256cda363b95e901d8e58bdb0c645
solito83 <solito83@users.noreply.github.com>
2021-01-21 21:51:26 -0300

    py-19

---
+++ modulos/des-019.py
@@ -0,0 +1,15 @@
+des = 'Sorteador'
+print ('{}'.format(des))
+#Um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+import random
+num = random.randint (1, 10)
+print (num)
+
+#import random
+#p = 'Pedro'
+#m = 'Maria'
+#h = 'Hugo'
+#J = 'Jessica'
+#nome = random.randrange(p, m, h, j)
+#print (nome)
