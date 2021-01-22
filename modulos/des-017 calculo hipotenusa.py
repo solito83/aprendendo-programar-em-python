f23da63c53aac1eba6dbb747442d450d5ce24eae
solito83 <solito83@users.noreply.github.com>
2021-01-22 14:11:46 -0300

    py-20

--- modulos/des-017 calculo hipotenusa.py
+++ modulos/des-017 calculo hipotenusa.py
@@ -2,11 +2,15 @@ des = 'Pitagoras: medidas de um triângulo'
 print ('{}'.format(des))
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa
 
-caop = int(input('Digito: '))
-caad = int(input('Digito: '))
-hipo = caop ** 2 + caad ** 2
+from math import hypot
+co = float(input('Digito: '))
+ca = float(input('Digito: '))
 fim = 'The End'
 
-print ('O cateto oposto é: {} unid.medida\nO cateto adjacente é: {} unid.medida\nA hipotenusa vale: {:.2f} unid.medida\n{}'.format(caop, caad, hipo ** (1 / 2), fim))
+#{1ª forma}
+#hi = (co ** 2 + ca ** 2) ** (1 / 2)
+#print ('O cateto oposto é: {} unid.medida\nO cateto adjacente é: {} unid.medida\nA hipotenusa vale: {:.2f} unid.medida\n{}'.format(co, ca, hi , fim))
 
-#from math import pow
+#{2ª forma}
+hi = hypot(co, ca)
+print ('A hipotenusa vale {:.2f} unid.madida\n{}'.format(hi, fim))
