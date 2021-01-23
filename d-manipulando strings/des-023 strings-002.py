d784aed395412a95bf269162eaf159055f9fa54e
solito83 <solito83@users.noreply.github.com>
2021-01-23 20:14:35 -0300

    py-025

---
+++ d-manipulando strings/des-023 strings-002.py
@@ -0,0 +1,8 @@
+des = 'Desafio-023 strings-002'
+print ('{}'.format(des))
+
+#...Faça um programa que leia um número de 0 a 9999 e mostre na telacada um dos digitos separados. ex: unidade; dezena; centena; milhar
+
+n1 = int(input('Digite um número: '))
+num = n1
+print (num)
