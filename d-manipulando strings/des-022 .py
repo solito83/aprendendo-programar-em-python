086c6b7de9506bc7d87cc6e45aef6fc2db104024
solito83 <solito83@users.noreply.github.com>
2021-01-23 19:00:08 -0300

    py-024

---
+++ d-manipulando strings/des-022 .py
@@ -0,0 +1,17 @@
+des = ('Desafio-022 manipulando strings-001')
+print ('{}'.format(des))
+#Crie um Programa que leia um nome completo de uma pessoa e mostre na tela: 
+#O nome com todas as letras maiúsculas
+#O nome com todas as letras minúsculas
+#Quantas letras ao todo sem considerar espaços
+#Quantas letras tem o primeiro nome.
+
+nom = str(input('Qual é seu nome: '))
+nome = nom
+print ('Meu nome é: {}'.format(nome))
+
+print ('Colacando o texto todo em maiúsculo: ',nome.upper(),'OK')
+print ('Colocando o texto todo em minúsculo: ',nome.lower(),'ok') 
+print ('Contando as letras e excluíndo os espaços da contagem ', len(nome.strip()), '<corrigir>')
+
+print ('Contando somente as letras do primeiro nome: =',len(nome[:6]),'letras <corrigir>')
--- d-manipulando strings/string frases.py
+++ d-manipulando strings/string frases.py
@@ -1,3 +1,5 @@
+#Nessa aula, vamos aprender operações com String no Python. As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
+
 frase = ('Aprendendo programar em python')
 #print (len(frase)
 print (frase[:30])
