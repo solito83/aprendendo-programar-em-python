347e07955ca44fe518f4aacf6888b288695f3bb0
solito83 <solito83@users.noreply.github.com>
2021-01-24 22:07:07 -0300

    py-029

--- d-manipulando strings/des-027 strings-006.py
+++ d-manipulando strings/des-027 strings-006.py
@@ -1,3 +1,9 @@
 des = 'Desafio-027 strings-006'
 print ('{}'.format(des))
-#
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nom = str(input('Digite um Nome: ')).strip()
+nome = nom.split()
+print ('Olá, muito prazer')
+print ('Seu 1º nome é: {}'.format(nome[0]))
+print ('Seu ultimo nome é: {}'.format(nome[len(nome)-1]))
