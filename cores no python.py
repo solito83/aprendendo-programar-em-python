ab8c321b02cb274852b794fdf95f321da1f88b5b
solito83 <solito83@users.noreply.github.com>
2021-02-01 18:01:08 -0300

    py-038

--- cores no python.py
+++
@@ -1,3 +0,0 @@
-# Cores
-
-print ('\033  [Olá, mundo!\033]n')
--- e-estruturas condicionais/des-034 condicional-008.py
+++ e-estruturas condicionais/des-034 condicional-008.py
@@ -1,3 +1,10 @@
 des = 'Desafio-034 estruturas condicionais'
 print ('{}'.format(des))
 #Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+salario = (float(input('Digite seu salário atual: ')))
+
+if salario <= 1250:
+    print ('comentarios', salario + (salario * (15 / 100)))
+else:
+    print ('comentarios', salario + (salario * (10 /100)))
